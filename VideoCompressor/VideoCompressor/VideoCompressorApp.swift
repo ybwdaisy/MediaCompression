12518b4cf62080f7a03cfdd41da29a9c27498a23
ybwdaisy <ybw_daisy@163.com>
2022-11-09 14:46:44 +0800

    feat: create project

---
+++ VideoCompressor/VideoCompressor/VideoCompressorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCompressorApp.swift
+//  VideoCompressor
+//
+//  Created by ybw-macbook-pro on 2022/11/9.
+//
+
+import SwiftUI
+
+@main
+struct VideoCompressorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
